0fa399db238c88a108e57aeb0bba88fea4a308f8
Mark Larson <markdlarson@hey.com>
2024-07-12 12:49:37 -0400

    Prep app.py placeholder for later production
    cleaning the slate for a fresh start after model is complete

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-'''Processing taxi ride data'''
-import pandas as pd
+'''Modeling taxi ride data'''
+from flask import Flask, request, render_template
+from datetime import datetime
 
-# Read from Parquet file into Pandas dataframe
-df = pd.read_parquet(
-    "fhvhv_tripdata_2024-04.parquet",
-    engine='pyarrow',
-    columns=['request_datetime', 'PULocationID', 'DOLocationID', 'base_passenger_fare',
-             'tolls', 'bcf', 'sales_tax', 'congestion_surcharge', 'airport_fee'],
-    storage_options=None
-)
+app = Flask(__name__)
 
-pd.options.display.show_dimensions = False  # Suppress noisy dimension info
+# Load the Model
 
-# View only airport dropoffs
-print(df.loc[df['DOLocationID'].isin([1, 132, 138])])
+# Set Up Route Endpoints
+
+# Prediction
+
+# Summary
